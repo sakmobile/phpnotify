8c52e05fad533975f3e0f532ce8c888d0963a149
sakmobile <sakmobile@users.noreply.github.com>
2020-06-08 14:58:42 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+ $lat = isset($_GET['lat']);
+ $long = isset($_GET['long']);
+define('LINE_API',"https://notify-api.line.me/api/notify");
+ 
+$token = "c8Pk0ZaizdVp2wHOTsl7NGfK8FHeaENPBqIklsoX5Ri"; //ใส่Token ที่copy เอาไว้
+$str = " lat ".$lat."<br /> "."long ".$long; //ข้อความที่ต้องการส่ง สูงสุด 1000 ตัวอักษร
+ 
+$res = notify_message($str,$token);
+print_r($res);
+function notify_message($message,$token){
+ $queryData = array('message' => $message);
+ $queryData = http_build_query($queryData,'','&');
+ $headerOptions = array( 
+         'http'=>array(
+            'method'=>'POST',
+            'header'=> "Content-Type: application/x-www-form-urlencoded\r\n"
+                      ."Authorization: Bearer ".$token."\r\n"
+                      ."Content-Length: ".strlen($queryData)."\r\n",
+            'content' => $queryData
+         ),
+ );
+ $context = stream_context_create($headerOptions);
+ $result = file_get_contents(LINE_API,FALSE,$context);
+ $res = json_decode($result);
+ return $res;
+}
+
+?>
